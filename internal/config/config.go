096d34b18e7212c3ab19fd03d47f19f678feab16
Lucas Macedo <lucasedumacedo@gmail.com>
2026-09-21 17:10:09 +0100

    fix: put to patch

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	// Version of the provider
-	Version = "0.1.1"
+	Version = "0.1.2"
 
 	// UserAgent is the user agent for the provider
 	UserAgent = "terraform-provider-rustrak/" + Version
